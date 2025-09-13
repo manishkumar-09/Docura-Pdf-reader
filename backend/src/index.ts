aa9e926b75e01e76493a6654eb59082cf87579af
Manish <iammanish0312@gmail.com>
2025-09-13 14:10:36 +0530

    root file

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { configs } from "./config/env";
+import helmet from "helmet";
+import router from "./routes/api.routes";
+import cors from "cors";
+const app = express();
+configs.PORT || 4000;
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+app.use("/api", router);
+
+app.listen(configs.PORT, (): void => {
+  console.log(`server is listening at Port ${configs.PORT}`);
+});
